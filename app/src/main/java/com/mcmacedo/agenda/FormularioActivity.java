2edfe5282b4ee903d92c6acc8d6d5c1216758095
Matheus Macedo <matheus.c.macedo@gmail.com>
2018-03-18 15:02:52 -0300

    Botao do formulario no Options menu, navegação e botão add aluno no list.

--- app/src/main/java/com/mcmacedo/agenda/FormularioActivity.java
+++ app/src/main/java/com/mcmacedo/agenda/FormularioActivity.java
@@ -2,6 +2,9 @@ package com.mcmacedo.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,14 +15,26 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+    }
 
-        Button botaoSalvar = findViewById(R.id.formulario_btn_salvar);
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
                 Toast.makeText(FormularioActivity.this,
-                        "Salvando...", Toast.LENGTH_SHORT).show();
-            }
-        });
+                        "Aluno Salvo.", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
